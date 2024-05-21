53df26f35742b6f818a5b899bcd8b17df3054efe
Freddy-m <fmarcosdev@gmail.com>
2024-05-21 12:23:58 -0400

    fmarcos.dev

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const URL_SITE = "https://fmarcos.dev";
 export const SITE_TITLE = "fmarcos";
 export const SITE_DESCRIPTION = "website";
 export const HOME_PAGE = "/about_me";
